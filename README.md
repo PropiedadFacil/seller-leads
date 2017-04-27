0ae50e6131f1daf9ec371aef2ce7a9d56149a566
Boris Macias <bgonzalez@ing.uchile.cl>
2017-04-27 20:55:59 -0300

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 [![security](https://hakiri.io/github/PropiedadFacil/seller-leads/master.svg)](https://hakiri.io/github/PropiedadFacil/seller-leads/master)
+
+##Setup
+
+1. Make sure rvm & ruby 2.4.0 is installed in your system https://rvm.io/
+2. Make sure you have postgres installed
+3. gem install bundler
+4. Start postgres: postgres -D /usr/local/var/postgres
+5. bundle install
+6. rails db:create
+7. rails db:migrate
+8. rails s
--- spec/features/submit_address_step_spec.rb
+++ spec/features/submit_address_step_spec.rb
@@ -1,13 +1,19 @@
-feature 'Submit address step' do
+feature 'User submits' do
   before(:each) do
     visit '/start'
   end
 
-  scenario 'User submits a formatted address' do
+  scenario 'a properly formatted address' do
     fill_in "address", with: '7331 E Bonita Dr, Scottsdale'
     click_button "Find my home"
 
     expect(page).to have_text("Is this the correct address?")
+  end
+
+  xit scenario 'an incomplete address' do
+  end
+
+  xit scenario 'an empty field' do
   end
 
 end
